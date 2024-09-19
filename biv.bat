eb10c8d4d3541d47f7856ef45c828d04ba1edc26
hgh086 <hgh086@users.noreply.github.com>
2024-09-19 12:35:24 +0800

    Update biv.bat

--- biv.bat
+++ biv.bat
@@ -1,5 +1,5 @@
 del iv.obj
 del tinyfiledialogs.obj
 del iv.exe
-cl iv.c tinyfiledialogs.c /I ..\raylib\include /I ..\ray\deps /EHsc /c
+cl iv.c tinyfiledialogs.c /I ..\raylib\include /EHsc /c
 link /RELEASE /OUT:iv.exe /LIBPATH:..\raylib\lib iv.obj tinyfiledialogs.obj iv.res user32.lib ole32.lib shell32.lib comdlg32.lib raylib.lib
